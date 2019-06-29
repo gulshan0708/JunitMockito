aa2ba10f8e917cb6013824d1df5685e6c7d9d268
gulshan <gulshankumar0708@gmail.com>
2019-06-30 00:28:18 +0530

    farzi commit

---
+++ JunitMockito/JunitMockito/src/main/java/org/gk/junitMockito/service/ResgistrationService.java
@@ -0,0 +1,16 @@
+package org.gk.junitMockito.service;
+
+public class ResgistrationService {
+
+	private static String fname;
+	private static String lname;
+	private static int id;
+
+	public String fullName() {
+		return fname+lname;
+	}
+
+	
+	
+	
+}
---
+++ JunitMockito/JunitMockito/src/test/java/org/gk/junitMockito/serviceTest/RegistrationServiceTest.java
@@ -0,0 +1,25 @@
+package org.gk.junitMockito.serviceTest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class RegistrationServiceTest {
+	private static final Logger LOG = LogManager.getLogger(RegistrationServiceTest.class);
+
+	@DisplayName("Test1")
+	@Test
+	public void testregService() {
+		LOG.info("This Will Be Printed On Info");
+
+		Assertions.assertEquals(1, 1);
+
+		LOG.info("Appending string: {}.", "Hello, World");
+	}
+
+}
